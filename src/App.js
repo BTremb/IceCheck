dd21e2858ffdc759ccbce9f45818f71f1859ecef
Patrick Costello <31824057+prcostello@users.noreply.github.com>
2023-05-02 08:56:03 -0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HelloWorld from './components/HelloWorld';
+import MaterialUI from './components/MaterialUI';
+import Stateful from './components/Stateful';
+import Conditional from './components/Conditional';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    /**
+     * BrowserRouter is a component that wraps the Routes component.
+     */
+    <BrowserRouter>
+      <Routes>
+          {/* 
+            The Route component is used to define a route.
+            The path prop is used to define the path of the route.
+            The element prop is used to define the component that will be rendered when the route is matched.
+          */}
+          <Route path="/" element={<HelloWorld />}/>
+          <Route path="/material-ui" element={<MaterialUI />}/>
+          <Route path="/deep-state" element={<Stateful />}/>
+          <Route path='/flipflop' element={<Conditional />}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 

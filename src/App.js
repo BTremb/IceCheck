8db1b75d33cabd82b5a6666e8c5285959c76aafc
BTremb <baileytremblett@hotmail.com>
2023-05-15 22:48:33 -0230

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HookForm from './components/HookForm';
+import MapView from './components/MapView';
 
 const App = () => {
   return (
  <BrowserRouter>
   <Routes>
-    <Route path="/" element={<HookForm />}/>
+    <Route path="/" element={<MapView />}/>
   </Routes>
  </BrowserRouter>
   );

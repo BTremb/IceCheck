0f20c4dc7a975d89754affa6e6ef4f07cfb7bd03
BTremb <baileytremblett@hotmail.com>
2023-05-23 12:40:31 -0230

    .

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,42 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Button from '@mui/material/Button';
 
 const NavBar = () => {
   const navBarStyles = {
     backgroundColor: '#f2f2f2',
-    padding: '0.6rem',
+    padding: '0.8rem',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     gap: '2rem',
   };
 
-  const buttonStyles = {
-    backgroundColor: '#4f9cba',
-    color: '#ffffff',
-    border: 'none',
-    padding: '10px 20px',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    marginRight: '10px',
+  const navigate = useNavigate();
+  const [flipFlop, setFlipFlop] = useState(false);
+
+  const buttonClick = (page) => {
+    setFlipFlop(!flipFlop);
+    navigate(page);
   };
 
-  const navigate = useNavigate();
+  const buttonStylesMapView = {
+    backgroundColor: flipFlop ? '#ff6f00' : '#4f9cba',
+    color: '#ffffff',
+  };
+
+  const buttonStylesListView = {
+    backgroundColor: flipFlop ? '#4f9cba' : '#ff6f00',
+    color: '#ffffff',
+  };
 
   return (
     <nav style={navBarStyles}>
-      <button style={buttonStyles} onClick={() => navigate('/')}>
+      <Button
+        style={buttonStylesMapView}
+        variant="contained"
+        onClick={() => buttonClick('/')}
+      >
         MapView
-      </button>
-      <button style={buttonStyles} onClick={() => navigate('/ListView')}>
+      </Button>
+      <Button
+        style={buttonStylesListView}
+        variant="contained"
+        onClick={() => buttonClick('/ListView')}
+      >
         ListView
-      </button>
+      </Button>
     </nav>
   );
 };
 
+export default NavBar;
 
-export default NavBar; 
+
+
+
 
 
 

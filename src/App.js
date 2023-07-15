08162bb644fe3c2d930d49784d17cfb6e0c2cf9a
BTremb <baileytremblett@hotmail.com>
2023-07-15 10:45:17 -0230

    change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import MapView from './components/Map/MapView';
 import ProfilePage from './components/forms/UserProfile';
@@ -8,18 +8,17 @@ import { UserProvider } from './contexts/UserContext';
 
 const IceCheck = () => {
   return (
-<UserProvider>
-  <BrowserRouter>
-    <NavBar />
-    <Routes>
-      <Route path="/" element={<MapView />} />
-      <Route path="/Profile" element={<ProfilePage />} />
-    </Routes>
-  </BrowserRouter>
-</UserProvider>
-
-
+    <UserProvider>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<MapView />} />
+          <Route path="/Profile" element={<ProfilePage />} />
+        </Routes>
+      </Router>
+    </UserProvider>
   );
-}
+};
 
 export default IceCheck;
+
